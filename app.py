ef44b21b7f1ef2b7a024bb8524eeb5b029830b99
Sukhman singh <sunnysaini062001@gmail.com>
2025-08-08 16:21:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -17,28 +17,24 @@ def splash_screen():
     st_lottie(lottie_data, speed=1, loop=True, quality="high")
     st.markdown("<h2 style='text-align:center;'>Loading The Carbonivore...</h2>", unsafe_allow_html=True)
 
-    # Save time when splash started
+# Main app logic
+def main():
     if "splash_start" not in st.session_state:
         st.session_state.splash_start = time.time()
 
-def main():
-    # Show splash screen for 3 seconds only on first run
-    if "splash_done" not in st.session_state:
+    # Show splash for 2 seconds
+    if time.time() - st.session_state.splash_start < 2:
         splash_screen()
-        if time.time() - st.session_state.get("splash_start", time.time()) > 3:
-            st.session_state.splash_done = True
-        return  # Stop rendering anything else during splash
-
-    # After splash is done
-    if st.session_state.get('user'):
-        main_app()
     else:
-        st.sidebar.title("Signin/Signup")
-        auth_choice = st.sidebar.radio("Select", ["Sign In", "Sign Up"])
-        if auth_choice == "Sign In":
-            signin()
+        if st.session_state.get('user'):
+            main_app()
         else:
-            signup()
+            st.sidebar.title("Signin/Signup")
+            auth_choice = st.sidebar.radio("Select", ["Sign In", "Sign Up"])
+            if auth_choice == "Sign In":
+                signin()
+            else:
+                signup()
 
 if __name__ == "__main__":
     main()
